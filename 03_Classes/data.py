cb4cc8a4f48d560da67537138f430665f00184b4
MN <magmarnowak@gmail.com>
2017-02-20 11:33:43 +0100

    Add data file

---
+++ 03_Classes/data.py
@@ -0,0 +1,24 @@
+class InstanceCounter(object):
+    count = 0                   # class attrubute = data shared among instances
+
+    def __init__(self, val):
+        self.val = val
+        InstanceCounter.count +=1 # access class attribute and increment it
+
+    def set_val(self, newval):
+        self.val = newval
+
+    def get_val(self):
+        return self.val
+
+    def get_count(self):
+        return InstanceCounter.count
+
+a = InstanceCounter(5)
+b = InstanceCounter(13)
+c = InstanceCounter(17)
+d = InstanceCounter(29)
+
+for obj in (a, b, c, d):
+    print "val of obj: %s" % (obj.get_val())
+    print "count: %s" % (obj.count)
