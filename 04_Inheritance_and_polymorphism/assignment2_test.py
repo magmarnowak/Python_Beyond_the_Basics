201b9ebdd47899a45e254e04a2e3d014e67e7f37
MN <magmarnowak@gmail.com>
2017-04-26 12:30:01 +0200

    Add scripts for assignment 2

--- 04_Inheritance_and_polymorphism/assignment2_test.py
+++ 04_Inheritance_and_polymorphism/assignment2_test.py
@@ -6,5 +6,6 @@ c = DelimFile('text.csv', ',')
 log.write('this is a log message')
 log.write('this is another log message')
 
-c.write(['a', 'b', 'c', 'd'])
+c.write(['a', 'b', 'c, c', 'd'])
+print c.output
 c.write(['1', '2', '3', '4'])
